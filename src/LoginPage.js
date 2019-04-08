6d34af15de292b45d617eac82e08a17804df2e68
srilakshmimounika1996 <lakshmimounika.sri@walkingtree.tech>
2019-04-08 19:02:24 +0530

    loginpage js

---
+++ src/LoginPage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./loginpage.css";
+class LoginPage extends React.Component{
+    constructor(props){
+        super(props);
+
+    }
+    render(){
+        return(
+            <div className="gridcontanier">
+        
+            <div className="gridcontanier1">
+            <div className="box">
+            <p className="boxText">Organising always seems impossible until its DONE!</p>
+            
+            </div>
+            </div>
+<div className="gridcontanier2">
+    </div>
+                </div>
+        );
+    }
+}
+export default LoginPage;
