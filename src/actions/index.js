8cc3d3ca9cc949c60b97a2f389b192a07403b5b6
mounikagudur <mounika.gudur@walkingtree.tech>
2019-04-09 12:18:35 +0530

    actions folder

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+//Action Creators
+
+export const signIn = (userId) => {
+    return {
+      type: "SIGN_IN",
+      payload: userId
+    };
+  };
+  
+  export const signOut = () => {
+    return {
+      type: "SIGN_OUT"
+    };
+  };
