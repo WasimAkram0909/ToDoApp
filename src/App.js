21453cab5e55b3e6990d23ac0dcfca207ed789ec
anil kumar reddy genupula <anil.kumar@walkingtree.tech>
2019-04-09 10:13:47 +0530

    component name changed images added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Welcome from './components/welcome';
-import LoginPage from './LoginPage';
+import Welcome from './components/Welcome';
 class App extends Component {
   render() {
     return (
