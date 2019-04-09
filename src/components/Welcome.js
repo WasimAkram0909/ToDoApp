03861764e2ae48297bd32d2890fd7d008f1d9609
anil kumar reddy genupula <anil.kumar@walkingtree.tech>
2019-04-09 13:04:22 +0530

    class name changed to Welcome. updated welcome.css

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,23 +4,27 @@ import profilePic from '../assets/ProfilePicture.png';
 import circleTick from '../assets/Success.png';
 import forwardArrow from '../assets/RightArrow.png';
 
-class App extends Component {
+class Welcome extends Component {
   render() {
     return (
-      <div className="App">
+        <main className="App">
+      <div className="AppDiv">
           <img src={profilePic} className="profilePic"/>
           <p className="welcome">Welcome </p>
           <div className="message">
             <img src={circleTick} className="circleTick"/>
             <p>You have Successfully Created ToDo Account </p>
           </div>
-          <button className="button"><img src={forwardArrow} className="arrow"/></button>
+          <button className="button">
+          <img src={forwardArrow} className="arrow"/>
+          </button>
 
       </div>
+      </main>
     );
   }
 }
 
 
 
-export default App;
+export default Welcome;
