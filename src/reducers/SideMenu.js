4adf0b4294392f77875f6bf8df65593cf9953c2b
mounikagudur <mounika.gudur@walkingtree.tech>
2019-04-22 14:45:25 +0530

    reducer

--- src/reducers/SideMenu.js
+++ src/reducers/SideMenu.js
@@ -2,22 +2,36 @@ export const SideMenuData = [
     {
         name:"Todo",
         image:require("../assets/Alltasks.svg"),
-        path:"Todo"
+        path:"Todo",
+        action:0,
     },
     {
         name:"CompletedTasks",
         image:require("../assets/CompletedTasks.svg"),
-        path:"CompletedTasks"
+        path:"CompletedTasks",
+        action:1,
+        
     },
     {
         name:"RescheduledTasks",
         image:require("../assets/Rescheduld_Tasks.svg"),
-        path:"RescheduledTasks"
+        path:"RescheduledTasks",
+        action:2,
+        
     },
     {
         name:"Profile",
         image:require("../assets/Profile.svg"),
-        path:"Profile"
+        path:"Profile",
+        action:3,
+        
     }
 ]
+// {
+//     ToDoAll,
+//      CompletedTaskAction,
+//     RescheduleTask,
+//      profileAction
+
+// }
     
