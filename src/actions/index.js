e301f624ffd5be6d3803198e81ddcd60edabae85
mounikagudur <mounika.gudur@walkingtree.tech>
2019-04-22 14:44:20 +0530

    actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,16 @@
 import axios from "axios";
-import ToDoAxios from "../api/ToDoaxios";
+// import ToDoAxios from "../api/ToDoaxios";
+// import {authToken} from '../components/googleauth'
 
+
+let ToDoAxios= axios.create({
+  baseURL:"http://115.248.119.138:8089/todo/",
+  headers:{
+  "Authorization":"ya29.Glz0BiLoVrwXNCge94nW2r6GHjVri7p8rt8YeaHLVVn6_kShd_0kJMed_Hm2jOlUqlkLP56zFqghrxuK4YiE6IPDWTZ3v3ZHqcYb8bmeKNa-15ZQExEzB7-xl4tfjg"
+  }
+  })
+
+//Action Creators
 export const signIn = (userId) => {
   return {
     type: "SIGN_IN",
@@ -14,37 +24,31 @@ export const signOut = () => {
   };
 };
 
-export const profileAction = (data) => {
-  // console.log(data,"actions");
-  var id = data.getId();
-  var fullName = data.getName();
-  var name = data.getGivenName();
-  var familyName = data.getFamilyName();
-  var image = data.getImageUrl();
-  var email = data.getEmail();
-  var logInData = {
-    id,
-    fullName,
-    name,
-    familyName,
-    image,
-    email
-  };
-  return {
-    type: "SIGN_IN_USER_DETAILS",
-    payload: logInData,
-  }
-}
-export const ToDoAll = () => {
-  console.log("data");
-  var url = "http://115.248.119.138:8089/todo/tasks";
-  return (dispatch) => {
-    return axios.get(url,
-      {
-        headers: {
-          "Authorization": " ya29.GlzzBkLT_OQBg3wM_RqlqMR1K-jaIoE33JaP8Qr6QjHpqcBgdgl6fs-Df1DigBQvhJFyvh4l7S0q80luTQQx6KVWs0C8hJOliLBWu2fAzFvDJcXyOXYsf9-C-38rUw"
-        }
-      })
+// export const profileAction = (data) => {
+//   // console.log(data,"actions");
+//   var id = data.getId();
+//   var fullName = data.getName();
+//   var name = data.getGivenName();
+//   var familyName = data.getFamilyName();
+//   var image = data.getImageUrl();
+//   var email = data.getEmail();
+//   var logInData = { id, fullName, name, familyName, image, email };
+//   return {
+//     type: "SIGN_IN_USER_DETAILS",
+//     payload: logInData,
+//   }
+// }
+
+  export const ToDoAll = () => {
+    console.log("data");
+    var url = "http://115.248.119.138:8089/todo/tasks";
+    return (dispatch) => {
+      return axios.get(url,
+        {
+          headers: {
+            "Authorization": " ya29.GlzzBkLT_OQBg3wM_RqlqMR1K-jaIoE33JaP8Qr6QjHpqcBgdgl6fs-Df1DigBQvhJFyvh4l7S0q80luTQQx6KVWs0C8hJOliLBWu2fAzFvDJcXyOXYsf9-C-38rUw"
+          }
+        })
       .then(res => {
         console.log(res);
         // console.log(res.data.main);
@@ -63,55 +67,31 @@ export const ToDoAllAction = (RES) => {
     payload: RES,
   };
 };
-// export const UpdateTask = () => {
-//   return {
-//     type: "UPDATE_TASK"
-//   };
-export const DeleteTask = (data) => {
-  console.log(data);
-
-//   return (dispatch) => {
-//     return ToDoAxios.delete(`tasks/1`)
-//       .then(res => {
-//         console.log(res)
-//         dispatch(DeleteAction(res));
-//       })
-//   }
-// }
-// const DeleteAction = (data) => {
-//   console.log(data);
+export const UpdateTask = () => {
   return {
-    type: "DELETE_TASK",
-    payload: data,
+    type: "UPDATE_TASK"
   };
 };
 
-// export const AddTask = () => {
-//   return {
-//     type: "ADD_TASK"
-//   };
-// };
-export const SaveTask = (data) => {
-  console.log(data.Task);
-  var url = `http://115.248.119.138:8089/todo/tasks?name=${data.Task}&date=${data.date}&userId=${1}`;
-  return (dispatch) => {
-    return axios.post(url)
-      .then(res => {
-        console.log(res);
-        console.log(res.data.task);
-        dispatch(SaveTaskAction(res.data.task));
+    export const profileAction =() =>{
+      // var url=`http://115.248.119.138:8089/todo/profile`;
+      return(dispatch)=>{
+        return  ToDoAxios.get(`/profile`)
+        .then(res=>{
+          console.log(res.data);
+          dispatch(GetProfile(res.data));
+        })
       }
-    )
+      
   }
-}
-export const SaveTaskAction = (dataid) => {
-  // console.log(dataid);
-  return {
-    type: "SAVE_TASK",
-    payload: dataid,
-  };
-
-};
+  const GetProfile = (Profiledata)=>{
+    console.log(Profiledata)
+    return{
+      type:"GET_PROFILE",
+      payload:Profiledata
+    }
+  }
+ 
 export const TasksApi = (data) => {
   console.log(data);
   var status = data.tasks.status;
@@ -129,76 +109,99 @@ export const TasksApi = (data) => {
   }
 }
 export const SortByAction = (data) => {
-
-
   return {
     type: "SORT_BY",
     payload: data,
   };
-};
-
-export const RescheduleTask = (data) => {
-
-
-  console.log(data);
-
-  //   var reschedule = `${url}/updateTasks/${data.index}`;
-  //   console.log(deleteThis);
-  //   return (dispatch)=>{
-  //       return axios.post(reschedule)
-  //       .then (res =>{
-  //         dispatch(rescheduleAction(res));
-  //       })
-  //   }
-  // }
-  //   const rescheduleAction=(data)=>{
-  //    console.log(data);
-
-
-  return {
-    type: "RESCHEDULE_TASK",
-    payload: data,
-  }
 }
 
-export const CompletedTaskAction = (data) => {
+  
+
+    export const EditProfile = (data) =>{
+      console.log(data.picture);
+      var url=`http://115.248.119.138:8089/todo/profile?firstname=${data.firstname}&lastname=${data.lastname}&picture=${data.picture}&userId=${1}`
+      return(dispatch)=>{
+        return axios.post(url,{
+          headers:{
+            "Authorization":"ya29.GlzwBqAbnEuanUZXKVIG7jxtB68anv7bRxGXgOx4CdX7aE6eu9NdPUZMmoHWeVGU3ok3tSIDTMWwq5JWFpo0a6HLPKzD6ZV-C1HU9D4Gz6BQzKwbDC9sgMchpmeRwg"
+          }
+        })
+    .then(res=>{
+        console.log(res);
+        dispatch(EditProfileAction(res.data));
+    })}
+  }
+      const EditProfileAction = (resData)=>{
+        return{
+          type:"EDIT_PROFILE",
+          payload:resData
+        }
+      }
+
+
+
+  export const DeleteTask = (data) => {
+    // console.log(data);
+    // var url = (`http://115.248.119.138:8089/todo/deleteTasks/?taskId=${0}`);
+    // return (dispath)=>{
+    //     return axios.delete(url)
+    //     .then (res =>{
+    //       console.log(res)
+    //       // dispatch();
+    //     })
+    // }
+    //  console.log(data);
+    return {
+      type: "DELETE_TASK",
+      payload:data,
+    };
+  };
+
+  export const SaveTask = (data) => {
+    console.log(data.taskName);
+    var url=`http://115.248.119.138:8089/todo/tasks?date=10-07-2019&name=${data.taskName}`
+    // var url=`http://115.248.119.138:8089/todo/tasks?name=${data.taskName}&date=${data.createDate}&userId=${1}`;
+    return(dispatch)=>{
+      return axios.post(url,{
+        headers:{
+          "Authorization":"ya29.GlzwBr8tPsT5rduF6erhifjPTHHX7oJmxOTM2o_PJ5LXqA1UbsG9qw5Ty7CAQM91YFNnrWgkBq90lYstkMJXXMW3sZVyNoD6Hq0aJYmtu36yWqzarUz9Xqb_XICuTA"
+        }
+      })
+  .then(res=>{
+      console.log(res);
+      console.log(res.data.task);
+      dispatch(SaveTaskAction(res.data.task));
+  })
+}
+  }
+ const SaveTaskAction = (dataid) => {
+    console.log(dataid);
+    return {
+      type: "SAVE_TASK",
+      payload: dataid,
+
+  }}
+
+  export const RescheduleTask =(data)=>{
+console.log(data);
+    return {
+      type:"RESCHEDULE_TASK",
+      payload:data,
+    }
+  }
+  export const CompletedTaskAction =(data)=>{
+console.log(data);
+    console.log("this action is triggerd form sidemenu");
+    return {
+      type:"COMPLETED_TASK",
+      payload:data,
+    }
   console.log(data);
-  // console.log(date)
-
-
-  //   var complete = `${url}/updateTasks/${data.index}`;
-  //   console.log(complete);
-  //   return (dispatch)=>{
-  //       return axios.post(reschedule)
-  //       .then (res =>{
-  //         dispatch(completedAction(res));
-  //       })
-  //   }
-  // }
-  //   const completedAction=(data)=>{
-  //    console.log(data);
-
-
-  return {
-    type: "COMPLETED_TASK",
-    payload: data,
   }
 
-}
 export const UndoAction = () => {
   return {
     type: "UNDO"
   }
 }
-export const EditProfile = (data) => {
-  // console.log(data);
-  return {
-    type: "EDIT_PROFILE",
-    payload: data
-  }
-}
-
-
-
-
 
